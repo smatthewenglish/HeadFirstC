8d95e422de23d6f525ad247f9084e98dcf100777
s.matthew.english <s.matthew.english@gmail.com>
2018-05-09 10:00:51 -0400

    2018.05.09

--- cards.c
+++ cards.c
@@ -4,40 +4,35 @@
 int main () 
 {
   char card_name[3];
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
-  // int val = 0;
-  // if (card_name[0] == 'K') {
-  //   val = 10;
-  // } else if (card_name[0] == 'Q') {
-  //   val = 10;
-  // } else if (card_name[0] == 'J') {
-  //   val = 10;
-  // } else if (card_name[0] == 'A') {
-  //   val = 11;
-  // } else {
-  //   val = atoi(card_name);
-  // }
-  int val = 0;
-  switch (card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-      val = 10;
-      break;
-    case'A':
-      val = 11;
-      break;
-    default:
-      val = atoi(card_name);
-  }
-  printf("The card value is: %i\n", val);
-
-  /* Check if the value is 3 to 6 */
-  if ((val > 2) && (val < 7))
-    puts("Count has gone up");
-  /* Otherwise check if the card was J, Q, or K */
-  else if (val == 10)
-    puts("Count has gone down");
+  int count = 0;
+  do {
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch (card_name[0]) {
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case'A':
+        val = 11;
+        break;
+      case 'X':
+        continue;
+      default:
+        val = atoi(card_name);
+        if ((val < 1) || (val > 10)) {
+          puts("I don't understand that value!");
+          continue;
+        }
+    }
+    if ((val > 2) && (val < 7)) {
+      count++;
+    } else if (val == 10) {
+      count--;
+    }
+    printf("The current count is: %i\n", count);
+  } while (card_name[0] != 'X');
   return 0;
 }
