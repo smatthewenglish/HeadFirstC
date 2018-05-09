b0b366631310ec4460c40549c47e5c2fe80260db
s.matthew.english <s.matthew.english@gmail.com>
2018-05-09 02:20:16 -0400

    2018.05.09

--- cards.c
+++ cards.c
@@ -6,17 +6,30 @@ int main ()
   char card_name[3];
   puts("Enter the card_name: ");
   scanf("%2s", card_name);
+  // int val = 0;
+  // if (card_name[0] == 'K') {
+  //   val = 10;
+  // } else if (card_name[0] == 'Q') {
+  //   val = 10;
+  // } else if (card_name[0] == 'J') {
+  //   val = 10;
+  // } else if (card_name[0] == 'A') {
+  //   val = 11;
+  // } else {
+  //   val = atoi(card_name);
+  // }
   int val = 0;
-  if (card_name[0] == 'K') {
-    val = 10;
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  } else if (card_name[0] == 'J') {
-    val = 10;
-  } else if (card_name[0] == 'A') {
-    val = 11;
-  } else {
-    val = atoi(card_name);
+  switch (card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case'A':
+      val = 11;
+      break;
+    default:
+      val = atoi(card_name);
   }
   printf("The card value is: %i\n", val);
 
