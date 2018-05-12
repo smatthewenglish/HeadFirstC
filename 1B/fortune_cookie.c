5728c68b6d7b310eefa6d453ebceffce32035f97
s.matthew.english <s.matthew.english@gmail.com>
2018-05-12 18:27:21 -0400

    2018.05.12

---
+++ 1B/fortune_cookie.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void fortune_cookie(char msg[])
+{
+  printf("Message reads: %s\n", msg);
+  printf("msg occupies %i bytes\n", sizeof(msg)); 
+}
+
+int main()
+{
+  char quote[] = "Cookies make you fat";
+  fortune_cookie(quote);
+  printf("The quote string is stored at: %p\n", quote);
+  return 0;
+}
