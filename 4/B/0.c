7b5d98b1eca5d67618a884f5e2fb76a647f62ba6
S. Matthew English <s.matthew.english@gmail.com>
2018-05-27 01:36:53 -0400

    2018.05.27

---
+++ 4/B/0.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+/* insert code below */
+
+
+
+/* ^^^ */
+
+	printf("A day on Mercury is %f hours\n", day);
+	return 0;
+}
+
+float mercury_day_in_earth_days()
+{
+	return 58.65;
+}
+
+int hours_in_an_earth_day()
+{
+	return 24;
+}
