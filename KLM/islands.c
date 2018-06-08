07fae32583a48b76404ea374613eae0fbe7915fd
S. Matthew English <s.matthew.english@gmail.com>
2018-06-08 19:49:20 -0400

    2018.06.08

---
+++ KLM/islands.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct island {
+	char *name;
+	char *opens;
+	char *closes;
+	struct island *next;
+} island;
+
+island amity = {"Amity", "09:00", "17:00", NULL};
+island craggy = {"Craggy", "09:00", "17:00", NULL};
+island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
+island shutter = {"Shutter", "09:00", "17:00", NULL};
+
+void display(island *start)
+{
+	island *i = start;
+	for (; i != NULL; i = i->next) {
+		printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
+	}
+}
+
+int main()
+{
+
+	amity.next = &craggy;
+	craggy.next = &isla_nublar;
+	isla_nublar.next = &shutter;
+
+	display(&amity);
+	
+	return 0;
+}
